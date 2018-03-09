e11413988f350511667aba67b1740dd2f0a65d12
Andre Guedes <andre.guedes@asos.com>
2018-03-09 16:44:06 +0000

    First commit

---
+++ MLM.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLM.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
